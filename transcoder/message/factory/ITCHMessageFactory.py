ba6ae8d75b4368456d272f9fc273f53fa9347d06
Mark Servidio <mark.servidio@gmail.com>
2023-03-10 10:12:24 -0500

    Setup tools linting clean up (#117)
    * fixes
    
    * Update version.py
    
    * Update pylint.yml
    
    * Update __init__.py
    
    * Update .pylintrc
    
    * clean up and bumping require linting score up
    
    * Update .pylintrc
    
    * Update pylint.yml
    
    * Update pylint.yml
    
    * headers

--- transcoder/message/factory/ITCHMessageFactory.py
+++ transcoder/message/factory/ITCHMessageFactory.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2022 Google LLC
+# Copyright 2023 Google LLC
 #
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
@@ -16,6 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# pylint: disable=invalid-name
 
 import struct
 
